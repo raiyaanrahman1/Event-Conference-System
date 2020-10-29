be292baa66448abf55c0b33af6a459aa3a89a922
nitin <nitin.mahtani@mail.utoronto.ca>
2020-10-29 17:24:28 -0400

    nitin made entities pt 5- all entities created

--- phase1/src/Entity/Message.java
+++ phase1/src/Entity/Message.java
@@ -2,6 +2,12 @@ package Entity;
 
 public class Message {
     private String content;
-    private String receiver;
-    private String sender;
+    private String receiver; //username of receiver
+    private String sender;  //username of sender
+
+    public Message(String content, String receiver, String sender) {
+        this.content = content;
+        this.receiver = receiver;
+        this.sender = sender;
+    }
 }
