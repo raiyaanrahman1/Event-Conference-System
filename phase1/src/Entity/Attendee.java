4da7506fc472ed09954e280198418b13519286fc
Airiset <eric2000pima@gmail.com>
2020-10-29 23:37:06 -0400

    Add sample Javadoc for Attendee.java
    I have written up some sample Javadoc for the
    Attendee class. It will be a good idea if we create
    it as we code, since it is more descriptive, and that
    way we have less work during reading week (we have
    to provide Javadoc for our submission).
    
    I also changed the name for two
    methods, so that its more descriptive. I find it
    better if the methods addSentMessages and
    addReceivedMessages are instead called addSentMessage
    and addReceivedMessage, respectively. This is because
    we are only passing one Message as parameter at a
    time. If we were passing a list, it would make more
    sense to mak it plural.

--- phase1/src/Entity/Attendee.java
+++ phase1/src/Entity/Attendee.java
@@ -2,13 +2,21 @@ package Entity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Attendee extends User{
+/**
+ * The Attendee class represents am attendee, a user that can attend events.
+ */
+public class Attendee extends User {
     private List<User> contacts;
     private List<Message> receivedMessages;
     private List<Message> sentMessages;
     private List<Event> eventList;
 
-    //Attendee Contructor
+    /**
+     * Creates a new attendee object.
+     *
+     * @param uname  the Attendee's username
+     * @param pword  the Attendee's password
+     */
     public Attendee(String uname, String pword){
         super(uname, pword);
         this.contacts = new ArrayList<>();
@@ -17,52 +25,97 @@ public class Attendee extends User{
         this.eventList = new ArrayList<>();
     }
 
+    /**
+     * Returns the broadcast rights flag for this attendee.
+     * This flag is always false, since an attendee cannot broadcast to events.
+     *
+     * @return  the attendee's broadcast rights
+     */
     @Override
     public boolean hasBroadcastRights() {
         return false;
     }
 
+    /**
+     * Returns the event creation rights flag for this attendee.
+     * This flag is always false, since only organizers can create events.
+     *
+     * @return  the attendee's event creation rights
+     */
     @Override
     public boolean hasEventCreatingRights() {
         return false;
     }
 
 
-    //contacts Getter
+    /**
+     * Returns this attendee's contacts.
+     *
+     * @return  the attende's contact list
+     */
     public List<User> getContacts() {
         return contacts;
     }
-    //Method that appends a contact to contacts
+
+    /**
+     * Adds user to contact list.
+     *
+     * @param user  the new contact for this attendee
+     */
     public void addContact(User user) {
         this.contacts.add(user);
     }
 
-
-    //receivedMessages getter
+    /**
+     * Returns the messages received by this attendee.
+     *
+     * @return  the list of received messages
+     */
     public List<Message> getReceivedMessages() {
         return receivedMessages;
     }
-    //Method that appends a message to receivedMessages
-    public void addReceivedMessages(Message receivedMessage) {
+
+    /**
+     * Adds a new message to this attendee's received messages.
+     *
+     * @param receivedMessage  the new message in this attendee's inbox
+     */
+    public void addReceivedMessage(Message receivedMessage) {
         this.receivedMessages.add(receivedMessage);
     }
 
-
-    //sentMessages getter
+    /**
+     * Returns all messages sent by this attendee.
+     *
+     * @return  the list of messages sent by this attendee
+     */
     public List<Message> getSentMessages() {
         return sentMessages;
     }
-    //Method that appends a message to sentMessages
-    public void addSentMessages(Message sentMessage) {
+
+    /**
+     * Adds new message sent by this attendee.
+     *
+     * @param sentMessage  the new message sent by this user
+     */
+    public void addSentMessage(Message sentMessage) {
         this.sentMessages.add(sentMessage);
     }
 
-
-    //eventList getter
+    /**
+     * Returns all events this attendee is registered to attend.
+     *
+     * @return  the list of events where this attendee is signed
+     */
     public List<Event> getEventList() {
         return eventList;
     }
-    //Method that appends an event to eventList
+
+    /**
+     * Adds new event for which this attendee has singed up.
+     *
+     * @param event  the event for which this event signed up
+     */
     public void addEvent(Event event) {
         this.eventList.add(event);
     }
