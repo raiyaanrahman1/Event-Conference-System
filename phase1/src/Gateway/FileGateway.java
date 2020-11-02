d463c63a46215c6d64121e7038b13c5d5c6ca5de
Alex Lomovtseva <allomovtseva@gmail.com>
2020-11-02 17:02:23 -0500

    Completed first draft of IGateway, FileGateway and FileGatewayTests.

--- phase1/src/Gateway/FileGateway.java
+++ phase1/src/Gateway/FileGateway.java
@@ -1,4 +1,37 @@
 package Gateway;
 
-public class FileGateway {
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileGateway implements IGateway{
+
+    private String filename;
+
+    public FileGateway(String fileName){
+        this.filename = fileName;
+    }
+
+    @Override
+    public String read() {
+        String text = "";
+        try {
+            text = new String(Files.readAllBytes(Paths.get(this.filename)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+    @Override
+    public void write(String content) {
+        try {
+            Files.write(Paths.get(this.filename), content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
