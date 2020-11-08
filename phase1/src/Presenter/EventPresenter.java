90ed5b4e0db3d4ea0f46d2f528fc914b1f35fdc3
Wyna193 <57465990+Wyna193@users.noreply.github.com>
2020-11-08 08:16:14 +0700

    Add main event menu that differ for A and O in EventPresenter

--- phase1/src/Presenter/EventPresenter.java
+++ phase1/src/Presenter/EventPresenter.java
@@ -1,4 +1,32 @@
 package Presenter;
 
+import Controller.EventManagementSystem;
+import UseCase.UserManager;
+
+import java.util.List;
+
 public class EventPresenter {
+    private UserManager user;
+    private EventManagementSystem events;
+
+    public void EventManagementSystem(UserManager user, EventManagementSystem events){
+        this.user = user;
+        this.events = events;
+    }
+
+    public void mainEventPage(){ //to be displayed for the overall menu page
+        attendeeEventMenu();
+        organizerEventMenu();
+    }
+    public void organizerEventMenu() {
+        List<String> userInfo = user.getUserInfoList();
+        if (userInfo.get(2).equals("O")) {
+            events.eventMenu();
+        }
+        System.out.println("You do not have access.");
+    }
+
+    public void attendeeEventMenu(){
+        events.eventMenu();
+    }
 }
