389ed003a1ae19810cac9f1bb184d1f4ff0d12c6
Airiset <eric2000pima@gmail.com>
2020-11-10 10:48:32 -0500

    Add sort functionality to MessageManager
    This commit adds a new private comparator class to the
    MessageManager class. It is used to compare two
    messages by their datetime. See the Javadoc for more
    detail.

--- phase1/src/UseCase/MessageManager.java
+++ phase1/src/UseCase/MessageManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Comparator;
 
 /**
  * The MessageManager class manages communications between users.
@@ -90,7 +91,7 @@ public class MessageManager {
             this.addUser(receiver);
         }
 
-        List<Message> messageList = messages.get(receiver);
+        List<Message> messageList = this.messages.get(receiver);
         messageList.add(message);
     }
 
@@ -101,7 +102,7 @@ public class MessageManager {
      * @return  the boolean flag representing the condition.
      */
     private boolean hasMessages(String receiver) {
-        return messages.containsKey(receiver);
+        return this.messages.containsKey(receiver);
     }
 
     /**
@@ -112,7 +113,45 @@ public class MessageManager {
      * @param receiver  the username of the user.
      */
     private void addUser(String receiver) {
-        messages.put(receiver, new ArrayList<>());
+        this.messages.put(receiver, new ArrayList<>());
     }
 
+    /**
+     * Sorts a list of messages by their
+     * @param messages  the list of messages to be sorted
+     */
+    private void sortMessages(List<Message> messages) {
+        messages.sort(new ComparatorByDateTime());
+    }
+
+    /**
+     * A comparator class used to compare messages by their datetime.
+     */
+    private class ComparatorByDateTime implements Comparator<Message> {
+
+        /**
+         * Compares two messages by their datetime.
+         *
+         * Between two messages, the most recent one is the greater one.
+         * The oldest one is the smallest one. If they occurred at the
+         * same time, they are the same.
+         *
+         * This comparator is not consistent with the equals method.
+         *
+         * @param mess1  the one message being compared
+         * @param mess2  the other message being compared
+         * @return 1 iff mess1 message is greater than mess2
+         *         -1 iff mess1 message is smaller than mess2
+         *         0 otherwise
+         */
+        public int compare(Message mess1, Message mess2) {
+            if (mess1.getDateTime().isAfter(mess2.getDateTime())) {
+                return 1;
+            } else if (mess1.getDateTime().isBefore(mess2.getDateTime())) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
 }
