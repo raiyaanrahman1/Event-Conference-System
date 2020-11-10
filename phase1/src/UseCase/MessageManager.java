e259ae6f767ba6ec4145b5599d971977a1ef85cd
tariq <tariq.kharseh@mail.utoronto.ca>
2020-11-10 10:26:57 -0500

    made the constructor for messagemanager public

--- phase1/src/UseCase/MessageManager.java
+++ phase1/src/UseCase/MessageManager.java
@@ -23,7 +23,7 @@ public class MessageManager {
     /**
      * Constructs default map, an empty one.
      */
-    private MessageManager() {
+    public MessageManager() {
         messages = new HashMap<>();
     }
 
