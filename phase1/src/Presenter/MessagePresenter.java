9ce36d15eb15cff53883cfc0ca9513db28af86a9
githubA327 <rosanallyayah@gmail.com>
2020-11-10 23:58:52 -0500

    Modified the MessagePresenter class.

--- phase1/src/Presenter/MessagePresenter.java
+++ phase1/src/Presenter/MessagePresenter.java
@@ -1,120 +1,132 @@
 package Presenter;
 
-import Controller.MessengerSystem;
-import Gateway.FileGateway;
-import Gateway.IGateway;
-import UseCase.UserManager;
+import java.util.List;
 import java.util.Scanner;
-
 
 public class MessagePresenter {
     Scanner myObj = new Scanner(System.in);
-    private UserManager user;
-    private MessengerSystem messenger;
-    IGateway g = new FileGateway("phase1/src/Controller/LogInInformation.txt");
 
-    public MessagePresenter(UserManager user, MessengerSystem messenger){
-        this.user = user;
-        this.messenger = messenger;
-    }
 
-    public void mainPage(){
+    public Integer mainPage(){
         boolean incorrectOption = false;
+        int answer;
         do {
             System.out.println("========== Messages Menu ========== \n" +
                     "1. Inbox \n" +
                     "2. Contact List \n" +
-                    "3. Add user to Contact List\n ");
+                    "3. Add user to Contact List\n");
             System.out.println("Input the number of the option you wish to choose:");
-            int answer = myObj.nextInt();
-            if (answer == 1){
-                mainInboxPage();
-            } else if (answer == 2){
-                mainContactPage();
-            } else if (answer == 3){
-                messenger.addUser();
-            } else {
+            answer = myObj.nextInt();
+            if (answer != 1 && answer != 2 && answer != 3){
                 System.out.println("You have entered an incorrect input. Please enter a valid input.");
                 incorrectOption = true;
             }
         } while (incorrectOption);
+        return answer;
     }
 
-    public void mainInboxPage(){
+
+    public Integer mainInboxPage(){
         boolean incorrectOption = false;
+        int answer;
         do {
-            System.out.println("========== Inbox Menu ========== \n" +
+            System.out.println("========== Inbox ========== \n" +
                     "1. View Received Messages \n" +
-                    "2. Back to Previous Menu");
+                    "2. Return to Previous Menu");
             System.out.println("Input the number of the option you wish to choose:");
-            int answer = myObj.nextInt();
-            if (answer == 1){
-                viewInbox();
-            }  else if (answer == 2){
-                mainPage();
-            } else {
+            answer = myObj.nextInt();
+            if (answer != 1 && answer != 2){
                 System.out.println("You have entered an incorrect input. Please enter a valid input.");
                 incorrectOption = true;
             }
         } while (incorrectOption);
+        return answer;
     }
 
-    public void viewInbox(){
-        String[][] messageBySenderTable = messenger.viewReceivedMessages();
+
+    public Integer viewInbox(List<String> inboxMessagesList){
+        int i = 1;
+        for(String msg: inboxMessagesList){
+            String[] msgarray = msg.split("\\|");
+            System.out.println(i + ". From " + msgarray[0] + " at " + msgarray[1] + " - " + msgarray[2]);
+            i++;
+        }
         boolean invalidInput = false;
+        int response;
         do {
-            System.out.println("Would you like to: \n 1. Reply \n 2. Go back to previous menu.");
-            int response = myObj.nextInt();
-            if (response == 1){
-                messenger.replyMessage(messageBySenderTable);
-            } else if (response == 2){
-                mainInboxPage();
-            } else{
+            System.out.println("Would you like to: \n " +
+                               "1. Reply to a Message\n " +
+                               "2. Go back to previous menu. \n");
+            response = myObj.nextInt();
+            if(response != 1 && response != 2){
                 System.out.println("You have entered an incorrect input. Please enter a valid input.");
                 invalidInput = true;
             }
-        }
-        while (invalidInput);
+        } while (invalidInput);
+        return response;
     }
 
 
-    public void mainContactPage(){
+    public Integer getSelectedMessageNumber(){
+        System.out.print("Select the message you want to reply to by entering its corresponding number.");
+        return myObj.nextInt();
+    }
+
+
+    public String getContent(){
+        System.out.print("Enter the content of your message.");
+        return myObj.nextLine();
+    }
+
+
+    public void formatContactList(List<String> contacts){
+        // Alphabetical sort
+        contacts.sort(null);
+        int i = 0;
+        for (String s : contacts){
+            i++;
+            System.out.println(i + ". " + s + "\n");
+        }
+    }
+
+
+    public Integer mainContactPage(){
         boolean incorrectOption = false;
-        messenger.formatContactList();
+        int answer;
         do {
             System.out.println("========== CONTACT LIST ========== \n" +
                                 "1. Select a Contact \n" +
                                 "2. Return to Previous Menu");
-            int answer = myObj.nextInt();
-            if (answer == 1){
-                boolean validContact = false;
-                String selectedContact;
-                int contactNumber;
-                do {
-                    System.out.println("Enter the number of the contact you would like to select.");
-                    contactNumber = myObj.nextInt();
-
-                    if (contactNumber > user.getContactList().size() || contactNumber == 0){
-                        System.out.println("Please entre a valid number.");
-                        validContact = true;
-                    }
-
-                }while(validContact);
-
-                selectedContact = user.getContactList().get(contactNumber - 1);
-                selectedContactPage(selectedContact);
-
-            } else if (answer == 2){
-                mainPage();
-            } else {
+            answer = myObj.nextInt();
+            if (answer != 1 && answer != 2){
                 System.out.println("You have entered an incorrect input. Please enter a valid input.");
                 incorrectOption = true;
             }
         } while (incorrectOption);
+        return answer;
     }
 
-    public void selectedContactPage(String selectedContact){
+
+    public Integer selectFromContactList(List<String> contacts){
+        boolean validContact = false;
+        int contactNumber;
+        do {
+            System.out.println("Enter the number of the contact you would like to select.");
+            contactNumber = myObj.nextInt();
+
+            if (contactNumber > contacts.size() || contactNumber == 0){
+                System.out.println("Please entre a valid number.");
+                validContact = true;
+            }
+
+        }while(validContact);
+        return contactNumber;
+    }
+
+
+    public Integer selectedContactPage(){
         boolean incorrectOption = false;
+        int answer;
         do{
             System.out.println("Would you like to: \n" +
                     "1. Send Message to Selected Contact\n" +
@@ -122,20 +134,36 @@ public class MessagePresenter {
                     "3. View Messages from a Selected Contact \n" +
                     "4. Return to Previous Menu");
             System.out.println("Input the number of the option you wish to choose:");
-            int answer = myObj.nextInt();
-            if (answer == 1){
-                messenger.messageAttendee();
-            } else if (answer == 2){
-                messenger.removeUser();
-            } else if (answer == 3){
-                messenger.getSpecificMessages(selectedContact);
-            } else if (answer == 4){
-                mainContactPage();
-            } else {
+            answer = myObj.nextInt();
+            if (answer != 1 && answer != 2 && answer != 3 && answer != 4) {
                 System.out.println("You have entered an incorrect input. Please enter a valid input.");
                 incorrectOption = true;
             }
         }while(incorrectOption);
+        return answer;
+    }
+
+
+    public Integer mainAddUserPage(){
+        boolean incorrectOption = false;
+        int answer;
+        do {
+            System.out.println("========== ADD USER ========== \n" +
+                    "1. Add a user \n" +
+                    "2. Return to Previous Menu");
+            answer = myObj.nextInt();
+            if (answer != 1 && answer != 2){
+                System.out.println("You have entered an incorrect input. Please enter a valid input.");
+                incorrectOption = true;
+            }
+        } while (incorrectOption);
+        return answer;
+    }
+
+
+    public String addUserPage(){
+        System.out.print("Please input the username of the user you wish to add:");
+        return myObj.nextLine();
     }
 
 }
