780299291ba306e8d9bc2dbaff598560a520aae3
nitin <nitin.mahtani@mail.utoronto.ca>
2020-11-11 12:43:14 -0400

    tests started lol

--- phase1/Tests/LoginSystemTests.java
+++ phase1/Tests/LoginSystemTests.java
@@ -1,5 +1,26 @@
 import static org.junit.Assert.*;
+
+import Controller.LoginSystem;
+import Entity.Attendee;
+import UseCase.MessageManager;
 import org.junit.*;
 
+import java.util.List;
+
 public class LoginSystemTests {
+
+    @Test(timeout = 50)
+    public void testWelcome() {
+        LoginSystem logIn = new LoginSystem();
+        logIn.welcome();
+        String message = "Hi bro, wanna hang";
+        Attendee uDavid = new Attendee("david", "1234");
+        Attendee uMark = new Attendee("david", "1234");
+
+        messageManager.message(uDavid, uMark, message);
+
+        List<String> messagesByDavid = messageManager.getMessages(uDavid, uMark);
+
+        assertTrue("message method not working", messagesByDavid.contains(message));
+    }
 }
