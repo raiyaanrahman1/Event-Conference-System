0142c72bd32fa2980ae4cf470c82aa8dc74e7965
tariq <tariq.kharseh@mail.utoronto.ca>
2020-11-12 21:27:53 -0500

    completed the javadoc for MessengerSystem

--- phase1/src/Controller/MessengerSystem.java
+++ phase1/src/Controller/MessengerSystem.java
@@ -7,17 +7,30 @@ import Presenter.MessagePresenter;
 import UseCase.MessageManager;
 import UseCase.UserManager;
 
+/**
+ * Manages the messages and the contact list of the logged on user.
+ * Communicates with the presenters.
+ */
 public class MessengerSystem {
 
     private UserManager user;
     private MessageManager msgMan;
     private MessagePresenter msgPres = new MessagePresenter();
 
+    /**
+     * Creates a MessengerSystem and initializes its UserManager and MessageManager.
+     *
+     * @param user the UserManager that the MessengerSystem communicates with
+     * @param msgMan the MessageManager that the MessengerSystem communicates with
+     */
     public MessengerSystem(UserManager user, MessageManager msgMan) {
         this.user = user;
         this.msgMan = msgMan;
     }
 
+    /**
+     * The main run method
+     */
     public void run() {
         boolean run = true;
         do {
@@ -38,6 +51,9 @@ public class MessengerSystem {
         } while (run);
     }
 
+    /**
+     * The run method for the mainContactPage.
+     */
     private void mainContactPageRun(Integer contactOption){
         boolean run = true;
         do {
@@ -51,6 +67,9 @@ public class MessengerSystem {
         } while (run);
     }
 
+    /**
+     * The run method for the mainSelectedContactPageRun.
+     */
     private void mainSelectedContactPageRun(String selectedUser, Integer option) {
         boolean run = true;
         do {
@@ -67,6 +86,9 @@ public class MessengerSystem {
         } while (run);
     }
 
+    /**
+     * The run method for the mainAddUserPageRun.
+     */
     private void mainAddUserPageRun(Integer option){
         boolean run = true;
         do {
@@ -79,22 +101,44 @@ public class MessengerSystem {
                 run = false;
             }
         } while (run);
-
     }
 
+    /**
+     * Gets the list of messages that the logged on user has received.
+     *
+     * @return a list of received messages.
+     */
     public List<String> viewReceivedMessages() {
         return msgMan.getMessages(user.getUserInfoList().get(0));
     }
 
+    /**
+     * Messages the user inputted with the content inputted.
+     *
+     * @param username the username of the User that will be messaged.
+     * @param content the content of the message.
+     */
     public void messageUser(String username, String content) {
         msgMan.message(user.getUserInfoList().get(0), username, content);
     }
+
+    /**
+     * From the list of received messages, reply to the message with the index inputted with the content inputted.
+     *
+     * @param number the index of the message from the list of received messages.
+     * @param content the content of the message.
+     */
     public void replyMessage(int number, String content) {
         String message = viewReceivedMessages().get(number - 1);
         String sender = message.split("\\|")[0];
         msgMan.message(user.getUserInfoList().get(0), sender, content);
     }
 
+    /**
+     * Add the user inputted to the logged on user's contact list.
+     *
+     * @param user2 the username of the user that will be added to the contact list.
+     */
     public boolean addUser(String user2){
         if (!user.getContactList().contains(user2) && user.getSignedUpUsers().contains(user2)) {
             user.addUserToContacts(user2);
@@ -103,14 +147,29 @@ public class MessengerSystem {
         return false;
     }
 
+    /**
+     * Remove the user from the logged on user's contact list.
+     *
+     * @param user2 the username of the user that will be added to the contact list.
+     */
     public void removeUser(String user2){
         user.removeUserFromContacts(user2);
     }
 
+    /**
+     * Gets the contact list of the user.
+     *
+     * @return the contact list
+     */
     public List<String> getContacts() {
         return user.getContactList();
     }
 
+    /**
+     * Gets the list of messages that the user had with the user inputted.
+     *
+     * @param username the username of the sender of the messages
+     */
     public List<String> viewMessages(String username) {
         // filter out the messages that were just sent by the sender
         List<String> lstofmessages = viewReceivedMessages();
