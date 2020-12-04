578eb3be9700aa1f0a8b1ce5e813502a1d0eb474
Airiset <eric2000pima@gmail.com>
2020-12-04 15:33:05 -0500

    Refactor MessengerSystem
    This commit refactors the MessengerSystem. Variable names are more readable,
    making extension and modification easier. The Javadoc now follows Java
    conventions.

--- phase2/src/Controller/MessengerSystem.java
+++ phase2/src/Controller/MessengerSystem.java
@@ -3,139 +3,35 @@ package Controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import Presenter.MessagePresenter;
 import UseCase.MessageManager;
 import UseCase.UserManager;
 
 /**
- * Manages the messages and the contact list of the logged on user.
- * Communicates with the presenters.
+ * A messenger system for the logged in user.
  */
 public class MessengerSystem {
 
-    private UserManager user;
-    private MessageManager msgMan;
-
+    private final UserManager userManager;
+    private final MessageManager messageManager;
 
     /**
-     * Creates a MessengerSystem and initializes its UserManager and MessageManager.
+     * Initializes the MessengerSystem.
      *
-     * @param user   the UserManager that the MessengerSystem communicates with
-     * @param msgMan the MessageManager that the MessengerSystem communicates with
+     * @param userManager  the manager object for users
+     * @param messageManager  the manager object for messages
      */
-    public MessengerSystem(UserManager user, MessageManager msgMan) {
-        this.user = user;
-        this.msgMan = msgMan;
+    public MessengerSystem(UserManager userManager, MessageManager messageManager) {
+        this.userManager = userManager;
+        this.messageManager = messageManager;
     }
 
-//    /**
-//     * Display the appropriate message menu depending on the type of logged-in user
-//     */
-//    public void menus() {
-//        if (user.getUserInfoList().get(2).equals("S")) {
-//            runSpeaker();
-//        } else {
-//            run();
-//        }
-//    }
-//
-//    /**
-//     * Runs the main message menu.
-//     */
-//    public void run() {
-//        boolean run = true;
-//        do {
-//            int option = msgPres.mainPage();
-//            if (option == 1) {
-//                int inboxOption = msgPres.mainInboxPage(viewReceivedMessages());
-//                if (inboxOption == 1) {
-//                    replyMessage(msgPres.getSelectedMessageNumber(), msgPres.getContent());
-//                }
-//            } else if (option == 2) {
-//                mainContactPageRun(msgPres.mainContactPage(getContacts()));
-//            } else if (option == 3) {
-//                mainAddUserPageRun();
-//            } else {
-//                run = false;
-//            }
-//        } while (run);
-//    }
-//
-//    /**
-//     * The main run method for a Speaker user
-//     */
-//    public void runSpeaker() {
-//        boolean run = true;
-//        do {
-//            int option = msgPres.mainSpeakerPage();
-//            if (option == 1) {
-//                int inboxOption = msgPres.mainInboxPage(viewReceivedMessages());
-//                if (inboxOption == 1) {
-//                    replyMessage(msgPres.getSelectedMessageNumber(), msgPres.getContent());
-//                }
-//            } else {
-//                run = false;
-//            }
-//        } while (run);
-//    }
-//
-//    private void mainContactPageRun(Integer contactOption) {
-//        boolean run = true;
-//        do {
-//            if (contactOption == 1) {
-//                String selectedUser = msgPres.selectFromContactList(getContacts());
-//                mainSelectedContactPageRun(selectedUser, msgPres.selectedContactPage());
-//                contactOption = msgPres.mainContactPage(getContacts());
-//            } else {
-//                run = false;
-//            }
-//        } while (run);
-//    }
-//
-//    private void mainSelectedContactPageRun(String selectedUser, Integer option) {
-//        boolean run = true;
-//        do {
-//            if (option == 1) {
-//                messageUser(selectedUser, msgPres.getContent());
-//                msgPres.successfulMessage();
-//                option = msgPres.selectedContactPage();
-//            } else if (option == 2) {
-//                removeUser(selectedUser);
-//                msgPres.removeUserSuccessful();
-//                run = false;
-//            } else if (option == 3) {
-//                msgPres.formatMessages(viewMessages(selectedUser));
-//                option = msgPres.selectedContactPage();
-//            } else {
-//                run = false;
-//            }
-//        } while (run);
-//    }
-//
-//    private void mainAddUserPageRun() {
-//        boolean run = true;
-//        do {
-//            Integer option = msgPres.mainAddUserPage();
-//            if (option == 1) {
-//                if (!addUser(msgPres.addUserPage())) {
-//                    msgPres.addUserError();
-//                } else {
-//                    msgPres.addUserSuccessful();
-//                    run = false;
-//                }
-//            } else {
-//                run = false;
-//            }
-//        } while (run);
-//    }
-
     /**
-     * Gets the list of messages that the logged on user has received.
+     * Gets the list of messages the logged on user has received.
      *
-     * @return a list of received messages.
+     * @return  the list of messages received by the logged in user
      */
     public List<String> viewReceivedMessages() {
-        return msgMan.getMessages(user.getUserInfoList().get(0));
+        return messageManager.getMessages(getLoggedInUser());
     }
 
     /**
@@ -144,29 +40,29 @@ public class MessengerSystem {
      * @return  a list of received messages
      */
     public List<String> viewArchivedMessages() {
-        return msgMan.getArchivedMessages(getLoggedInUser());
+        return messageManager.getArchivedMessages(getLoggedInUser());
     }
 
     /**
      * Messages the user inputted with the content inputted.
      *
-     * @param username the username of the User that will be messaged.
-     * @param content  the content of the message.
+     * @param username  the username of the User that will be messaged
+     * @param content  the content of the message
      */
     public void messageUser(String username, String content) {
-        msgMan.message(user.getUserInfoList().get(0), username, content);
+        messageManager.message(getLoggedInUser(), username, content);
     }
 
     /**
-     * From the list of received messages, reply to the message with the index inputted with the content inputted.
+     * Replies to the given message.
      *
-     * @param number  the index of the message from the list of received messages.
-     * @param content the content of the message.
+     * @param index  the index of the message in list of received messages
+     * @param content  the content of the message
      */
-    public void replyMessage(int number, String content) {
-        String message = viewReceivedMessages().get(number - 1);
+    public void replyMessage(int index, String content) {
+        String message = viewReceivedMessages().get(index - 1);
         String sender = message.split("\\|")[0];
-        msgMan.message(user.getUserInfoList().get(0), sender, content);
+        messageManager.message(userManager.getUserInfoList().get(0), sender, content);
 //        msgPres.successfulMessage();
         System.out.println("Success");
     }
@@ -177,60 +73,81 @@ public class MessengerSystem {
      * @param index  the index of the message in the list
      */
     public void archiveMessage(int index) {
-        msgMan.archive(getLoggedInUser(), viewReceivedMessages().get(index - 1));
+        messageManager.archive(getLoggedInUser(), viewReceivedMessages().get(index - 1));
     }
 
     /**
-     * Add the user inputted to the logged on user's contact list.
+     * Adds the given user to the logged in user's contact list.
      *
-     * @param user2 the username of the user that will be added to the contact list.
+     * @param username  the username of the given user
      */
-    public boolean addUser(String user2) {
-        if (!user.getUserInfoList().get(0).equals(user2) && !user.getContactList().contains(user2) && user.getSignedUpUsers().contains(user2)) {
-            user.addUserToContacts(user2);
-
+    public boolean addUser(String username) {
+        if (!getLoggedInUser().equals(username)
+                && !inContacts(username)
+                && userManager.getSignedUpUsers().contains(username)) {
+            userManager.addUserToContacts(username);
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
     /**
-     * Remove the user from the logged on user's contact list.
+     * Removes the given user from the logged in user's contact list.
      *
-     * @param user2 the username of the user that will be added to the contact list.
+     * @param username  the username of the given user
      */
-    public void removeUser(String user2) {
-        user.removeUserFromContacts(user2);
+    public void removeUser(String username) {
+        userManager.removeUserFromContacts(username);
     }
 
     /**
      * Gets the contact list of the user.
      *
-     * @return the contact list
+     * @return  the contact list
      */
     public List<String> getContacts() {
-        return user.getContactList();
+        return userManager.getContactList();
     }
 
     /**
-     * Gets the list of messages that the user had with the user inputted.
+     * Gets the list of messages sent by given the user to the current logged in
+     * user.
      *
-     * @param username the username of the sender of the messages
+     * @param  username the username of the sender of the messages
      */
     public List<String> viewMessages(String username) {
-        List<String> lstofmessages = viewReceivedMessages();
-        List<String> msgsfromsender = new ArrayList<>();
-        for (String msg : lstofmessages) {
-            String[] msgarray = msg.split("\\|");
-            if (msgarray[0].equals(username)) {
-                msgsfromsender.add(msg);
+        List<String> messages = viewReceivedMessages();
+        List<String> messagesBySender = new ArrayList<>();
+
+        for (String message : messages) {
+            String[] tokens = message.split("\\|");
+            if (tokens[0].equals(username)) {
+                messagesBySender.add(message);
             }
         }
-        return msgsfromsender;
+
+        return messagesBySender;
     }
 
+    /*
+     * Returns the username of the current logged in user.
+     *
+     * @return  the username of the logged in user
+     */
     private String getLoggedInUser() {
-        return user.getUserInfoList().get(0);
+        return userManager.getUserInfoList().get(0);
+    }
+
+    /*
+     * Returns true iff the given user is in the current logged in user's contact
+     * list.
+     *
+     * @param username  the given user's username
+     * @return  true iff the given user is the logged in user's contact list
+     */
+    private boolean inContacts(String username) {
+        return userManager.getContactList().contains(username);
     }
 
 }
